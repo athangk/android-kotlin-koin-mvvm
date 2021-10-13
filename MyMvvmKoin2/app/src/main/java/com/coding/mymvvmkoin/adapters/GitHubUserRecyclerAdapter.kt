dd0c6532b70a1e60350a13b57e4f7536cd0c2451
athangk <me@me.gr>
2021-10-13 12:14:34 +0300

    Refactor on recycler view adapter

--- MyMvvmKoin2/app/src/main/java/com/coding/mymvvmkoin/adapters/GitHubUserRecyclerAdapter.kt
+++ MyMvvmKoin2/app/src/main/java/com/coding/mymvvmkoin/adapters/GitHubUserRecyclerAdapter.kt
@@ -1,63 +1,59 @@
 package com.coding.mymvvmkoin.adapters
 
 
-import android.content.Context
-import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.*
-import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.coding.mymvvmkoin.databinding.GithubUserItemBinding
 import com.coding.mymvvmkoin.models.GitHubUserModel
 import com.squareup.picasso.Picasso
-import androidx.lifecycle.ViewModel
 
-class GitHubUserRecyclerAdapter(private var listData: ArrayList<GitHubUserModel>, private val mContext:Context) : RecyclerView.Adapter<GitHubUserRecyclerAdapter.ViewHolder>() {
+
+class GitHubUserRecyclerAdapter() :
+    ListAdapter<GitHubUserModel, GitHubUserRecyclerAdapter.GiHubUserViewHolder>(
+        GitHubUserDiffCallback
+    ) {
 
     private lateinit var binding: GithubUserItemBinding
-    lateinit var viewContext: Context
 
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        viewContext = parent.context
-
-        binding = GithubUserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        holder.githubUserName.text = listData[position].username
-        Picasso.with(mContext).load(listData[position].avatarURL).into(holder.githubUserImage);
-
-    }
-
-
-    override fun getItemCount(): Int {
-        return listData.size
-    }
-
-
-    fun clearData() {
-        listData.clear()
-        notifyDataSetChanged()
-    }
-
-    fun updateData(updatedUserData: ArrayList<GitHubUserModel>) {
-        listData.clear()
-        listData = updatedUserData
-        notifyDataSetChanged()
-    }
-
-    class ViewHolder(binding: GithubUserItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class GiHubUserViewHolder(binding: GithubUserItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         var githubUserName: TextView = binding.githubUserName
         var githubUserImage: ImageView = binding.githubUserImage
 
 
+        fun bind(gitHubUserModel: GitHubUserModel) {
+            var currentGitHubUser = gitHubUserModel
+            githubUserName.text = currentGitHubUser.username
+            Picasso.get().load(currentGitHubUser.avatarURL).into(githubUserImage);
+        }
     }
 
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GiHubUserViewHolder {
+        binding = GithubUserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GiHubUserViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: GiHubUserViewHolder, position: Int) {
+
+        val currentUser = getItem(position)
+        holder.bind(currentUser)
+
+    }
+}
+
+object GitHubUserDiffCallback : DiffUtil.ItemCallback<GitHubUserModel>() {
+    override fun areItemsTheSame(oldItem: GitHubUserModel, newItem: GitHubUserModel): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: GitHubUserModel, newItem: GitHubUserModel): Boolean {
+        return oldItem.username == newItem.username
+    }
 }
--- MyMvvmKoin2/app/src/main/java/com/coding/mymvvmkoin/ui/main/views/MainFragment.kt
+++ MyMvvmKoin2/app/src/main/java/com/coding/mymvvmkoin/ui/main/views/MainFragment.kt
@@ -2,19 +2,15 @@ package com.coding.mymvvmkoin.ui.main.views
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.*
 import com.coding.mymvvmkoin.adapters.GitHubUserRecyclerAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.coding.mymvvmkoin.databinding.MainFragmentBinding
 import com.coding.mymvvmkoin.models.GitHubUserModel
-import com.coding.mymvvmkoin.utilities.MY_MVVM_TAG
 
 class MainFragment : Fragment() {
 
@@ -42,7 +38,7 @@ class MainFragment : Fragment() {
         mainFragmentBinding.noResults = true
 
         mainFragmentBinding.btnGetUsers.setOnClickListener {
-            resetRecyclerList()
+            adapter.submitList(arrayListOf())
             mainFragmentBinding.noResults = false
             mainFragmentBinding.isLoading = true
             Handler().postDelayed({
@@ -53,7 +49,7 @@ class MainFragment : Fragment() {
 
         mainViewModel.userList.observe(viewLifecycleOwner, { it ->
             if (it.isNotEmpty()) {
-                updateRecyclerList(it)
+                adapter.submitList(it)
             } else {
                 mainFragmentBinding.noResults = true
             }
@@ -67,7 +63,7 @@ class MainFragment : Fragment() {
     private fun initRecyclerList(userRecyclerList: ArrayList<GitHubUserModel>) {
 
         mainFragmentBinding.userRecyclerList.visibility = View.VISIBLE
-        adapter = GitHubUserRecyclerAdapter(userRecyclerList, requireContext())
+        adapter = GitHubUserRecyclerAdapter()
         mainFragmentBinding.userRecyclerList.setHasFixedSize(true)
         val mLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
         mainFragmentBinding.userRecyclerList.layoutManager = mLayoutManager
@@ -77,14 +73,6 @@ class MainFragment : Fragment() {
         val snapHelper: SnapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(mainFragmentBinding.userRecyclerList)
 
-    }
-
-    private fun resetRecyclerList() {
-        adapter.clearData()
-    }
-
-    private fun updateRecyclerList(userRecyclerList: ArrayList<GitHubUserModel>) {
-        adapter.updateData(userRecyclerList)
     }
 
 
